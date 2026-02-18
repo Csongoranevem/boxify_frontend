b790adb7eb66bae127c94775602c21b9dbb2082c
Csongoranevem <czakocsongor@turr.hu>
2026-02-18 08:57:10 +0100

    prfil módosítás

--- src/app/components/user/update-profile/update-profile.component.ts
+++ src/app/components/user/update-profile/update-profile.component.ts
@@ -11,11 +11,13 @@ import { AuthService } from '../../../services/auth.service';
 import { PasswordModule } from 'primeng/password';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { DatePipe } from '@angular/common';
+import { User } from '../../../interfaces/user';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-update-profile',
   standalone: true,
-  imports: [FormsModule, InputTextModule, ButtonModule,InputGroupAddonModule,InputGroup,MenuModule, PasswordModule, FloatLabelModule, DatePipe],
+  imports: [FormsModule, InputTextModule, ButtonModule, InputGroupAddonModule, InputGroup, MenuModule, PasswordModule, FloatLabelModule, DatePipe],
   templateUrl: './update-profile.component.html',
   styleUrl: './update-profile.component.scss'
 })
@@ -23,46 +25,58 @@ export class UpdateProfileComponent implements OnInit {
 
   constructor(
     private api: ApiService,
-    private auth: AuthService
+    private auth: AuthService,
+    private messageService: MessageService
   ) { }
 
-  isEmailInputDisabled:boolean=true;
-  isNameInputDisabled:boolean=true;
-  userDetails: object = {
+  isEmailInputDisabled: boolean = true;
+  isNameInputDisabled: boolean = true;
+  userDetails: User = {
+    id: '',
     name: '',
     email: '',
-    lastLogin: '',
-    registrationDate: ''
+    password: '',
+    confirm: '',
+    role: '',
+    secret: '',
+    status: true,
+    reg: new Date(),
+    last: new Date(),
   };
 
   async ngOnInit(): Promise<void> {
     await this.api.selectById("users", this.auth.GetLoggedUser().id).subscribe(profile => {
-      console.log(profile)
-      this.userDetails = profile;
+      this.userDetails = profile as User;
     });
   }
 
-  Emailicon="pi pi-pencil";
-  Nameicon="pi pi-pencil";
-  toggleEmailInput()
-  {
+  Emailicon = "pi pi-pencil";
+  Nameicon = "pi pi-pencil";
+  toggleEmailInput() {
     this.Emailicon = this.isEmailInputDisabled ? "pi pi-times" : "pi pi-pencil";
     this.isEmailInputDisabled = !this.isEmailInputDisabled;
   }
 
-  toggleNameInput()
-  {
+  toggleNameInput() {
     this.Nameicon = this.isNameInputDisabled ? "pi pi-times" : "pi pi-pencil";
     this.isNameInputDisabled = !this.isNameInputDisabled;
   }
 
 
-  saveChanges()
-  {
+  saveChanges() {
     this.api.update("users", this.auth.GetLoggedUser().id, {
-      //email: this.isEmailInputDisabled ? null : this.userDetails.email,
-      //name: this.isNameInputDisabled ? null : this.userDetails.name
+      email: this.userDetails.email,
+      name: this.userDetails.name
+    }).subscribe({
+      next: (response) => {
+        this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Profile updated successfully!', life: 3000 });
+      },
+      error: (err) => {
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Error updating profile!', life: 3000 });
+      }
     });
+
+  
   }
 
 
@@ -72,5 +86,32 @@ export class UpdateProfileComponent implements OnInit {
     const confirmNewPassword = (document.getElementById("ConfirmNewpasswordField") as HTMLInputElement).value;
 
     return oldPassword !== "" && newPassword !== "" && newPassword === confirmNewPassword;
+  }
+
+
+  updatePassword() {
+    if (this.isPasswordValid()) {
+      const oldPassword = (document.getElementById("OldpasswordField") as HTMLInputElement).value;
+      const newPassword = (document.getElementById("NewpasswordField") as HTMLInputElement).value;
+
+      this.api.update("users/updatepass", this.auth.GetLoggedUser().id, {
+        oldPassword: oldPassword,
+        newPassword: newPassword
+      }).subscribe({
+        next: (response) => {
+          this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Password updated successfully!', life: 3000 });
+          (document.getElementById("OldpasswordField") as HTMLInputElement).value = "";
+          (document.getElementById("NewpasswordField") as HTMLInputElement).value = "";
+          (document.getElementById("ConfirmNewpasswordField") as HTMLInputElement).value = "";
+        },
+        error: (err) => {
+          this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Error updating password!', life: 3000 });
+        }
+      });
+    }
+    else {
+      this.messageService.add({ severity: 'error', summary: 'Hiba', detail: 'Töltsd ki az összes jelszó mezőt!', life: 3000 });
+
+    }
   }
 }
